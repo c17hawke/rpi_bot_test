6d0ce89de3b2456c2cda3d1344e01ea67ba15545
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2022-04-06 20:33:39 +0530

    pygame update

--- pygame_test.py
+++ pygame_test.py
@@ -4,11 +4,11 @@ from pygame.locals import *
 pygame.init()
 
 while(True):
-   for event in pygame.event.get():
-      if (event.type == KEYDOWN):
-         print (event)
-         if (event.key == K_KP0):
-			print ("numpad 0")
-      if (event.type == KEYUP):
-          print("stopping")
-          break
+    for event in pygame.event.get():
+        if (event.type == KEYDOWN):
+            print (event)
+            if (event.key == K_KP0):
+                print ("numpad 0")
+        elif (event.type == KEYUP):
+            print("stopping")
+            break
