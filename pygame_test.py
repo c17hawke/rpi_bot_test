e537b2d16e9bc61cadbd1b657674b061b24361d4
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2022-04-06 20:32:23 +0530

    pygame test

---
+++ pygame_test.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+while(True):
+   for event in pygame.event.get():
+      if (event.type == KEYDOWN):
+         print (event)
+         if (event.key == K_KP0):
+			print ("numpad 0")
+      if (event.type == KEYUP):
+          print("stopping")
+          break
